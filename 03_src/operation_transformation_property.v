180eb03685ba54e561f8de9847c349c9f245ee9a
Ryu <sato.ryunosuke.q4@dc.tohoku.ac.jp>
2025-09-22 15:30:02 +0900

    関数deleteに位置情報xを保持するための新しいデータ型を追加

--- 03_src/operation_transformation_property.v
+++ 03_src/operation_transformation_property.v
@@ -6,13 +6,75 @@ Import ListNotations.
 
 Check (1 ::2:: 3 :: []).
 
-Context {A B C : Type} {X : Type}.
+Class Eq A :=
+  {
+    eqb : A -> A -> bool
+  }.
+
+Context {A: Type} `{Eq A}.
+
+
+(* 任意の型Aに対して関数Ins を定義*)
+Fixpoint insert (A : Type) (p : nat) (x : A) (l : list A) : option (list A) :=
+  match p, l with
+  | 0, _ => Some (x :: l)
+  | S p', [] => None
+  | S p', h :: t =>
+      match insert A p' x t with
+      | None => None
+      | Some l' => Some ( h :: l')
+      end
+  end.
+  
+Compute insert nat 0 9 [1].
+Compute insert bool 2 true [false;false;false].
+
+(* 削除した要素の位置情報xを持つdeleted_elem型を導入 *)
+Inductive deleted_elem (A : Type) : Type :=
+ | Deleted (x : A) (l : list A)
+ | NotFound.
+
+Fixpoint delete (A : Type) (p : nat) (x : A) (l : list A) `{Eq A} : deleted_elem A :=
+  match p, l with
+  | 0, h :: t => Deleted A h t
+  | 0, [] => NotFound A
+  | S p', h :: t =>
+      match delete A p' x t with
+      | Deleted _ x l'  => Deleted A x (h :: l')
+      | NotFound _  => NotFound A
+      end
+  | S p', [] => NotFound A
+  end.
+
+Compute delete nat 0 1 [1;2;3].
+Compute delete nat 5 3 [1;2;3]. (* リストの長さ以上の要素に対する操作に対して、"None"を返す*)
+Compute delete bool 0 true [true;false;true].
+
+(* 操作の型を定義する型コンストラクタ *)
+Inductive Op (A : Type) :=
+ | OpIns : nat -> A -> list A -> Op A
+ | OpDel : nat -> list A -> Op A.
+
+(* 操作に対して、解釈関数interp_op を定義 *)
+Definition interp_op (A : Type) (op : Op A) (l : list A) : option (list A) :=
+  match op with
+  | OpIns _ p x l => insert _ p x l
+  | OpDel _ p l => delete _ p l
+  end.
+
+(* 操作間の変換関数it_op を定義 *)
+(*Definition it_op :=*)
+
+(* delete の引数に削除する文字xを追加することで元のデータに戻れるようにする必要がある*)
+(* しかし、そうなるとEq型クラスで要素の等価性を調べることができるような実装が必要*)
+(* p365で用いられるunit型 ならその実装が不要。*)
 
 Definition flip {A B C : Type} (f : A -> B -> C) : B -> A -> C :=
   fun b a => f a b.
 
 (* 2つの操作が逆に適用された場合に関数interp を適用するための関数 *)
 (** ここでは bind を用いずに記述しているが変更可 **)
+(** 部分適用の形で型を明示的に記述 **)
 Definition exec_all (f : C -> X -> option X) : list C -> option X -> option X :=
   List.fold_left (flip (fun c acc => 
     match acc with
@@ -20,28 +82,6 @@ Definition exec_all (f : C -> X -> option X) : list C -> option X -> option X :=
      | Some x => f c x
     end
   )).
-
-(*Definition g x y : nat := 
-  Nat.add (x * 3) y.
-
-Definition h x : nat -> nat :=
-  Nat.add (x * 3).
-
-Compute g 1 2.
-Compute h 1 2.
-*)
-
-(* exec_all の型、定義する際には型を一部明示せずともいいってこと？
- exec_all
-     : (C -> X -> option X) -> list C -> option X -> option X 
-*)
-
-Check exec_all.
-
-Definition test_exec_all_nat (op : nat -> nat) (x : nat) : nat :=
-  exec_all (fun op x => Some (op x))
-
-Compute exec_all 
 
 Class OTBase (X cmd : Type) :=
  {
@@ -51,75 +91,3 @@ Class OTBase (X cmd : Type) :=
      interp op1 m = Some m1 -> interp op2 m = Some m2 ->
       let m21 := 
  }
-
-(*Inductive op :=
-|Int : nat -> nat -> list nat -> list nat
-|Del : nat  -> list nat -> list nat.
- *)
-Print option.
-
-Fixpoint Ins (p x : nat) (l : list nat) : option (list nat) :=
-  match p, l with
-  | 0, _ => Some (x :: l)
-  | S p', [] => None
-  | S p', h :: t =>
-      match Ins p' x t with
-      | None => None
-      | Some l' => Some ( h :: l')
-      end
-  end.
-  
-Compute Ins 0 9 [1].
-Compute Ins 9 1 [1;2;3].
-Compute Ins 2 9 [1;2;3;4].
-
-(* パターンマッチの演習 *)
-Definition head (A : Type) (l : list A) : option A :=
-  match l with
-  | x :: _ => Some x
-  | [] => None
-  end.
-
-Compute head nat [1;2;3].
-
-Definition rtn_bind_value (x y : nat) : option nat :=
-  match y with
-  | 0 => None
-  | S y' => Some (y') 
-  end.
-
-Compute rtn_bind_value 10 2. (* yに2が渡されて、2はS 1にとパターンマッチする。-> y'に1が束縛される *)
-Compute rtn_bind_value 10 3.
-Compute rtn_bind_value 10 4.
-
-Definition safe_div (x y : nat) : option nat :=
-  match y with
-  | 0 => None (* unable to divide *)
-  | S y' => Some (x / S y')
-  end.
-
-Compute safe_div 10 2.
-  
-Fixpoint Del (p : nat) (l : list nat) : option (list nat) :=
-  match p, l with
-  | 0, _ :: t => Some t
-  | 0, [] => None
-  | S p', h :: t =>
-      match Del p' t with
-      | Some l'  => Some (h :: l')
-      | None  => None
-      end
-  | S p', [] => None
-  end.
-
-Compute Del 0 [1;2;3].
-Compute Del 0 [].
-Compute Del 2 [1;2;3].
-Compute Del 5 [1;2;3].
-
-
-
-
-
-
-  
