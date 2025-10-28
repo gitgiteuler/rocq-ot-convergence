d7b69dda87ec1977e15afa7977496fb3af4cb5b7
Ryu <sato.ryunosuke.q4@dc.tohoku.ac.jp>
2025-10-29 02:53:54 +0900

    補題ot_convergence_property_c1とそのインスタンスの作成（証明は省略）

--- 03_src/coq-project/operation_transformation_property.v
+++ 03_src/coq-project/operation_transformation_property.v
@@ -26,6 +26,8 @@ Instance eqBool : Eq bool :=
 
 (*Context {A: Type} `{Eq A}.*)
 
+Compute eqb 5 10. 
+Compute eqb true false.
 
 (* 任意の型Aに対して関数Ins を定義*)
 Fixpoint insert [A : Type] (p : nat) (x : A) (l : list A) : option (list A) :=
@@ -64,17 +66,22 @@ Inductive Op (A : Type) :=
  | OpIns : nat -> A ->  Op A
  | OpDel : nat -> A -> Op A.
 
-About OpIns.
+About OpIns. 
 
 Arguments OpIns [A%_type_scope] _%_nat_scope _.
 Arguments OpDel [A%_type_scope] _%_nat_scope _.
 
 (* 操作opをリストlを適用する関数 *)
-Definition interp_op [A : Type] `{Eq A}  (op : Op A) (l : list A) : option (list A) :=
+Definition interp_op [A : Type] `{Eq A} (op : Op A) (l : list A) : option (list A) :=
   match op with
   | OpIns p x => insert p x l
   | OpDel p x => delete p x l
   end.
+
+Check interp_op.
+About interp_op.
+
+Arguments interp_op {A} {H} op l.
 
 (* ある操作に対して、その逆の操作を行う関数 *)
 Definition inv_op [A : Type] `{Eq A} (op : Op A) : Op A :=
@@ -83,31 +90,19 @@ Definition inv_op [A : Type] `{Eq A} (op : Op A) : Op A :=
   | OpDel p x => OpIns p x
   end.
 
-Definition list_ex := (1 :: 2 :: 3 :: []).
-Compute (interp_op (OpIns 0 0) list_ex). (* => [0; 1; 2; 3] *)
-Compute (inv_op (OpIns 0 0)).  
-Compute (inv_op (OpDel 0 1)).
+Definition it_sc {A : Type} `{Eq A} (op1 op2 : Op A) (f : bool) : list(Op A) :=
+  (* f=true の場合はクライアント優先、f=false の場合はサーバ優先 *)
+  if f then []
+  (* f=falseの時、サーバの操作を優先するため、まずクライアントの操作op2を逆操作で元に戻し（inv op2）、その後サーバの操作op1を適用する。
+     これにより、サーバ側の状態を優先しつつ、クライアントの操作が競合した場合でも一貫性を保つことができる。操作順序は「クライアントの操作を巻き戻してからサーバの操作を適用する」ことを意図している。*)
+  else [inv_op op2 ; op1]. 
 
-Definition it_op_sc {A : Type} `{Eq A} (op1 op2 : Op A) (f : bool) : list(Op A) :=
-  if f then [] (* f=trueの時、空リストを返す *)
-  else [inv_op op2 ; op1]. (* f=falseの時、サーバの操作を優先する為、クライアントの操作op2を戻して、op1を行うという操作のリストを返す *) 
-
-Compute it_op_sc (OpIns 1 1) (OpIns 2 2) true.
-Compute it_op_sc (OpIns 1 1) (OpIns 2 2) false.
+Compute it_sc (OpIns 1 1) (OpIns 2 2) true.
+Compute it_sc (OpIns 1 1) (OpIns 2 2) false.
 
 Compute Nat.ltb 1 2. (* 1 < 2 と言う命題が”真” *)
 Compute S 0.
 Compute Nat.eqb (S 0) 1.
-
-(* op1, op2 の操作の優先度（順番）を表す値が必要 => サイトID（sid）を使う？ *)
-(*Definition it_op_pair {A : Type} `{Eq A} (op1 op2 : Op A) : list (Op A) :=
-  match op1, op2 with
-  | Ins p1 x1, Ins p2 x2 => if Nat.ltb p1 p2 then [Ins p1 x1]
-                            else if Nat.ltb p2 p1 then [Ins (p1 + 1) x1]
-                            else (* p1 == p2 位置の競合が起きる場合 *)
-                              if 
-*)
-
 
 (* 全ての操作をリストに適用する関数 *)
 Fixpoint exec_all_op [A : Type] `{Eq A} (ops : list(Op A)) (l : list A): option (list A) :=
@@ -119,34 +114,36 @@ Fixpoint exec_all_op [A : Type] `{Eq A} (ops : list(Op A)) (l : list A): option 
                         end
   end.
 
-(* 一時的にOtDevから引用しているコード *) 
-(** foldl を fold.left に変更 **)
-(*Context {C : Type} {X : Type}.*)
-(*Definition exec_all (f : C -> X -> option X) := fold_left (flip (fun c => bind (f c))).*)
-
-Compute exec_all_op [OpIns 0 0 ; OpDel 1 1 ; OpDel 1 2] [1;2;3].
-
-Check OTBase.
+Lemma ot_convergence_property_c1 (A : Type) `{Eq A} :
+  forall (op1 op2 : Op A) (f : bool) m (m1 m2 : list A),
+    interp_op op1 m = Some m1 ->
+    interp_op op2 m = Some m2 ->
+    let m21 := (exec_all interp_op) (Some m2) (it_sc op1 op2 f) in
+    let m12 := (exec_all interp_op) (Some m1) (it_sc op2 op1 (negb f)) in
+      m21 = m12 /\ exists node, m21 = Some node.
+Proof.
+  intros.
+  split.
+  unfold m21, m12.
+  unfold it_sc.
+  unfold inv_op.
+  destruct f.
+    simpl.
+Admitted.
 
 Instance OTBaseListSC {A : Type} `{Eq A} : OTBase (list A) (Op A) :=
 {
   interp := @interp_op A _;
-  it := @it_op_sc A _;
-  it_c1 :=
-    fun (op1 op2 : Op A) (f : bool) (m m1 m2 : list A)
-        (H1 : interp_op op1 m = Some m1)
-        (H2 : interp_op op2 m = Some m2) =>
-
-        let m21 := exec_all_op (it_op_sc op1 op2 f) m2 in
-        let m12 := exec_all_op (it_op_sc op2 op1 (negb f)) m1 in
-        
-        (* 証明のスキップ *)
-        admit
+  it := @it_sc A _;
+  it_c1 := fun op1 op2 f m m1 m2 =>
+          ot_convergence_property_c1 A op1 op2 f m m1 m2;
 }.
 
+(* TODO:型クラスOTInv のインスタンスの作成に必要な補題ot_inv_propertyの証明 *)
 Class OTInv [A : Type] `{Eq A} :=
 {
-  inv_op : Op A -> Op A
-  ip1 : forall (op : Op A) (l l' : list A), 
-    interp_op op l = Some l' -> interp_op (inv_op op) l' = Some l
+  inv := @inv_op 
+  ip1 : forall op m mr, interp op m = Some mr -> interp (inv op) mr = (Some m)
 }.
+
+(* TODO:型クラスOTInv のインスタンスの作成 -> 証明 *)
